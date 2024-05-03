f81db7636e56e7e6b4a1b0bceff0b92a1bbd79f6
Static <angiebusinessacc@gmail.com>
2024-05-03 08:43:08 +0200

    dying

---
+++ main.py
@@ -0,0 +1,20 @@
+''' 1. Vytvořte proměnnou se svým či vymyšleným jménem.
+2. Vypište své jméno s malými písmeny a počet výskytu písmena „j“
+3. Vytvořte funkci, která umožní porovnávat hesla zda se shodují či ne.
+4. Vytvořte seznam s 5 hodnotami kde budou jak čísla tak string.
+5. Vypište první, prostřední a poslední hodnotu seznamu,
+6. Zaměňte 2, 3 a 5 hodnotu v seznamu za „auto“, 89 a „zelená“.'''
+
+
+
+
+x = "Static TvHead"
+print(x.lower())   
+print(x.count('j'))
+
+
+z = (2, ' bee', 69, 'moth', 0)
+print(x[0], x[2], x[4])
+x[1] = 'auto'
+x[2] = 89
+x[4] = 'zelená'
